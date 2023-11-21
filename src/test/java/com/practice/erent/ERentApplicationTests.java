3d4570c343c8860b5a709721b9f91af2c012eb8d
Juan Lucero <jm.lucero1389@gmail.com>
2023-11-21 10:42:34 -0300

    First Commit

---
+++ src/test/java/com/practice/erent/ERentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.erent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
